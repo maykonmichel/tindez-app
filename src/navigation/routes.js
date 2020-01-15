7aff9319b2b641ee017a9995e90bff9de70f4c69
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-15 12:07:19 -0200

    Added routes

---
+++ src/navigation/routes.js
@@ -0,0 +1,9 @@
+import {createSwitchNavigator} from 'react-navigation';
+
+import Login from '../screens/Login';
+import Main from '../screens/Main';
+
+export default createSwitchNavigator({
+  Login,
+  Main,
+});
