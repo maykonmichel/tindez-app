10d49dc9c88cc0884747bae758d88057a88ed196
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-15 13:14:45 -0200

    Added logout option

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {View, Image} from 'react-native';
+import React, {useCallback} from 'react';
+import {Image, TouchableOpacity, View} from 'react-native';
 
 import logo from '../assets/images/logo.png';
 
-export default () => {
+export default ({navigation: {navigate}}) => {
+  const onLogout = useCallback(() => navigate('login'), [navigate]);
+
   return (
     <View
       style={{
@@ -12,7 +14,9 @@ export default () => {
         justifyContent: 'space-between',
         backgroundColor: '#f5f5f5',
       }}>
-      <Image source={logo} />
+      <TouchableOpacity onPress={onLogout}>
+        <Image source={logo} />
+      </TouchableOpacity>
     </View>
   );
 };
