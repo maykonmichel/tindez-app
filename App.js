b80843bff525d0aeab4e9c7531bfe8e7faee5535
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-15 11:35:58 -0200

    Added user value watcher

--- App.js
+++ App.js
@@ -1,52 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 import logo from './src/assets/images/logo.png';
 
-export default () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: '#f5f5f5',
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 30,
-    }}>
-    <Image source={logo} />
+export default () => {
+  const [user, setUser] = useState('');
 
-    <TextInput
-      autoCapitalize="none"
-      placeholder="Digite seu usuário no Github"
+  return (
+    <View
       style={{
-        marginTop: 20,
-        paddingHorizontal: 15,
-        height: 45,
-        alignSelf: 'stretch',
-        backgroundColor: '#FFF',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 4,
-      }}
-    />
-
-    <TouchableOpacity
-      style={{
-        marginTop: 10,
-        height: 45,
-        alignSelf: 'stretch',
-        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#f5f5f5',
         justifyContent: 'center',
-        backgroundColor: '#DF4723',
-        borderRadius: 4,
+        alignItems: 'center',
+        padding: 30,
       }}>
-      <Text
+      <Image source={logo} />
+
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Digite seu usuário no Github"
+        value={user}
+        onChangeText={setUser}
         style={{
-          color: '#ffffff',
-          fontWeight: 'bold',
-          fontSize: 16,
+          marginTop: 20,
+          paddingHorizontal: 15,
+          height: 45,
+          alignSelf: 'stretch',
+          backgroundColor: '#FFF',
+          borderWidth: 1,
+          borderColor: '#ddd',
+          borderRadius: 4,
+        }}
+      />
+
+      <TouchableOpacity
+        style={{
+          marginTop: 10,
+          height: 45,
+          alignSelf: 'stretch',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#DF4723',
+          borderRadius: 4,
         }}>
-        Enviar
-      </Text>
-    </TouchableOpacity>
-  </View>
-);
+        <Text
+          style={{
+            color: '#ffffff',
+            fontWeight: 'bold',
+            fontSize: 16,
+          }}>
+          Enviar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
